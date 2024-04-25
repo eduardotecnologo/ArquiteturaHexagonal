1b7f8a426cf30882ecf5d856d153c11e182ce22b
eduardotecnologo <eduardotecnologo@hotmail.com.com>
2024-04-25 14:18:02 -0300

    feat: create Spring projecT

---
+++ jurosboleto/src/main/java/br/com/jurosboleto/EduardoDeveloperApplication.java
@@ -0,0 +1,13 @@
+package br.com.jurosboleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduardoDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduardoDeveloperApplication.class, args);
+	}
+
+}
